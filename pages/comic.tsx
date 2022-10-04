d1679e22090215f1c3f85f04e4e296ff76bb24a7
CodedCypher <51363964+CodedCypher@users.noreply.github.com>
2022-10-04 18:37:53 +0400

    fix the comic

--- pages/comic.tsx
+++ pages/comic.tsx
@@ -4,7 +4,7 @@ const Comic = () => {
 	return (
 		<Container size="md" sx={{ minHeight: "100vh" }}>
 			{Array.from(Array(9).keys()).map((_, i) => (
-				<Image src={`/comic/Page ${i + 1}-min.jpg`} key={i} alt={`Page ${i + 1}`} />
+				<Image src={`/comic/Page ${i + 1}.jpg`} key={i} alt={`Page ${i + 1}`} />
 			))}
 		</Container>
 	);
